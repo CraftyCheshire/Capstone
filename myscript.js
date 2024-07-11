3a6acbe8a79dff96bf05324f33181ff65302e38b
craftycheshire <mini_vann@ymail.com>
2024-07-11 17:23:30 -0500

    update

---
+++ myscript.js
@@ -0,0 +1,5 @@
+function myFunction() {
+  document.getElementById("yolo").style.fontSize = "35px"; 
+  document.getElementById("yolo").style.color = "yellow";
+  document.getElementById("yolo").style.backgroundColor = "red";        
+}
