e43c531777d6b27eedf67b03a436e164cd7be0c0
craftycheshire <mini_vann@ymail.com>
2024-07-15 21:54:47 -0500

    update

--- myscript.js
+++ myscript.js
@@ -3,5 +3,6 @@ function myFunction() {
   document.getElementById("changeUp").style.color = "yellow";
   document.getElementById("changeUp").style.backgroundColor = "red";        
 }
+
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
