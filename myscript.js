66a89b68ba9f8574a22357df9c1a5b97881b1bed
craftycheshire <mini_vann@ymail.com>
2024-07-16 20:33:47 -0500

    update

--- myscript.js
+++ myscript.js
@@ -6,3 +6,13 @@ function myFunction() {
 
 const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+let count = 0;
+const button = document.getElementById('counterButton, counterButton1, counterButton2, counterButton3, counterButton4, counterButton5');
+const countDisplay = document.getElementById('count');
+
+button.addEventListener('click', () => {
+  count++;
+  countDisplay.textContent = count;
+});
+
